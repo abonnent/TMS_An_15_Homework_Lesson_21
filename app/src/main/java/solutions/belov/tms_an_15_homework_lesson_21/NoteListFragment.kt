94f1e9620d4faa00248a5d42322786cae800b0f1
abonnent <abonnent@inbox.ru>
2023-10-23 16:48:46 +0300

    twenty-first lesson

--- app/src/main/java/solutions/belov/tms_an_15_homework_lesson_21/NoteListFragment.kt
+++ app/src/main/java/solutions/belov/tms_an_15_homework_lesson_21/NoteListFragment.kt
@@ -1,13 +1,16 @@
 package solutions.belov.tms_an_15_homework_lesson_21
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import solutions.belov.tms_an_15_homework_lesson_21.databinding.FragmentNoteListBinding
 
 class NoteListFragment : Fragment() {
@@ -23,25 +26,37 @@ class NoteListFragment : Fragment() {
         binding = FragmentNoteListBinding.inflate(inflater, container, false)
         val view = binding.root
 
-
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        adapter = NoteAdapter(notesList)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.addItemDecoration(SpaceItemDecoration(16))
 
         viewModel = ViewModelProvider(requireActivity()).get(NotesViewModel::class.java)
         viewModel.getNotes().observe(viewLifecycleOwner, Observer { notes ->
-            //notesList.clear()
-            notesList.addAll(notes)
-            Log.d("ZZZ", notes.toString())
-            Log.d("ZZZZ", notesList.toString())
+            if (notes.isNotEmpty()) {
+                with(binding) {
+                    noNotes.isVisible = false
+                    recyclerView.isVisible = true
+                }
 
-            //adapter.notifyItemInserted(notesList.size - 1)
+                notesList.clear()
+                notesList.addAll(notes)
+                adapter.notifyItemInserted(notesList.lastIndex)
+            }
         })
 
-        adapter = NoteAdapter(notesList)
-        binding.recyclerView.adapter = adapter
-        adapter.notifyDataSetChanged()
+        adapter.setOnItemClickListener { note ->
+            val message = "Заметка:\n${note.title}\n${note.text}"
 
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, message)
+            }
 
-        //adapter.setOnItemClickListener { note ->
-        //}
+            val chooser = Intent.createChooser(sendIntent, getString(R.string.note_send))
+            startActivity(chooser)
+        }
 
         return view
     }
---
+++ app/src/main/java/solutions/belov/tms_an_15_homework_lesson_21/SpaceItemDecoration.kt
@@ -0,0 +1,19 @@
+package solutions.belov.tms_an_15_homework_lesson_21
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SpaceItemDecoration(private val space: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        outRect.left = space
+        outRect.right = space
+        outRect.top = space
+        outRect.bottom = space
+    }
+}
